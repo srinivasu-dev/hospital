2742e076cd27f6cf44d3227991d1d3bda289c682
root <root@ip-172-31-8-181.ap-south-1.compute.internal>
2023-08-10 08:29:45 +0000

    task

---
+++ sonar-project.js
@@ -0,0 +1,21 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://15.207.71.102:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.login': 'squ_1287b67671c7358d8ff0eec90bfb1a48d2171bda',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+     }, () => {});
+
+
